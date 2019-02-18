f628f18b2e993cb3112788a7d3c99cd315516538
MSturges <Sturges3232@gmail.com>
2019-02-18 12:07:41 -0700

    subscriptions working

--- graphql-api/schema/Message/resolvers.js
+++ graphql-api/schema/Message/resolvers.js
@@ -1,17 +1,41 @@
+import { withFilter } from "apollo-server";
+import { pubsub } from "../subscriptions";
+
 const CreateMessageQuery = require("../../database/queries/message/CreateMessage");
 const GetChatGroupQuery = require("../../database/queries/chatGroup/getChatGroup");
 const GetUserQuery = require("../../database/queries/user/GetUser");
+
+// may want to change this to be unique to the group
+// may want to refactor to using redis
+const MESSAGE_ADDED_TOPIC = "messageAdded";
 
 export const resolvers = {
   Mutation: {
     createMessage(_, args) {
       return CreateMessageQuery(args)
         .then(data => {
+          // publish the message to our pupsub
+          // data is message
+          pubsub.publish(MESSAGE_ADDED_TOPIC, {
+            [MESSAGE_ADDED_TOPIC]: data
+          });
           return data;
         })
         .catch(err => {
           console.log("mongo error", err);
         });
+    }
+  },
+  Subscription: {
+    messageAdded: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(MESSAGE_ADDED_TOPIC),
+        (payload, args) => {
+          return Boolean(
+            args.userId != payload.messageAdded.from // don't send to user creating message
+          );
+        }
+      )
     }
   },
   Message: {
